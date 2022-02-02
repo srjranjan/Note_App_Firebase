71a989a06c3312873aef0cf234d929f63b90ca77
SRJ_ranjan <sudhanshuwithyoualll@gmail.com>
2022-02-02 23:59:14 +0530

    Connected to firebase FireStore DB
    Signed-off-by: SRJ_ranjan <sudhanshuwithyoualll@gmail.com>

--- app/src/main/java/com/srj/noteappfirebase/MainActivity.kt
+++ app/src/main/java/com/srj/noteappfirebase/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.srj.noteappfirebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
+    var db = FirebaseFirestore.getInstance()
+    private val TAG: String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Ada"
+        user["last"] = "Lovelace"
+        user["born"] = 1815
+
+// Add a new document with a generated ID
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    TAG,
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
     }
 }
